0f73f41edfcd132079d40d63a18b74b2b5da6cca
Miamia <doanngocmai2000@gmail.com>
2023-11-17 11:23:51 +0700

    bai2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Trang chu</title>
+    <meta charset="utf-8">
+    <\head>
+
+<body>
+    <?php
+		echo "Xin chao cac ban!";
+	?>
+    <\body>
